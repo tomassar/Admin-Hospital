cdd60a721c68d8f33e12265045439bfba09b29c9
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2021-11-04 18:13:18 -0300

    cambios

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -32,6 +32,7 @@ class CreateNewUser implements CreatesNewUsers
             'email' => $input['email'],
             'phone' => $input['phone'],
             'address' => $input['address'],
+            'usertype' => $input['usertype'],
 
             'password' => Hash::make($input['password']),
         ]);
--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
         'email',
         'phone',
         'address',
+        'usertype',
         'password',
     ];
 
